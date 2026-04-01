c333b8b9bfaaebe00051ac325ac3bd364dff04e5
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-01 09:38:55 +0530

    rewrite: add sqlalchemy models for user and scrobble.

--- src/memoryfm/cli/__init__.py
+++
@@ -1,7 +0,0 @@
-from platformdirs import user_data_path
-
-base_dir = user_data_path(appname="memoryfm", appauthor=False, ensure_exists=True)
-imports_dir = base_dir / "imports"
-imports_file = base_dir / "imports.json"
-loaded_file = base_dir / "loaded.json"
-API_KEY = "ec7d47f4fad9db5bcdff094a1243c582"
