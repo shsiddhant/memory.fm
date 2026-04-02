d487ca858ef855cc49f446f9d90b37c53d9353fb
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-02 12:42:27 +0530

    db: set Foreign keys ON in sqlite db.

--- src/memoryfm/db.py
+++ src/memoryfm/db.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 import logging
 from contextlib import contextmanager
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
+from sqlite3 import Connection as SQLite3Connection
 
 from memoryfm.config import DB_URL
 from memoryfm.core.models import Base
@@ -12,10 +13,20 @@ if TYPE_CHECKING:
     from sqlalchemy import Engine
 
 engine: Engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 logging.basicConfig()
 logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+
+
+@event.listens_for(engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 
 def init_db():
