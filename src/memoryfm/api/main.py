7a0b3806d64157d9f9155b9725deef77d074ef79
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-03 19:38:48 +0530

    enh: add a last.fm sync endpoint and sync progress websocket.

--- src/memoryfm/api/main.py
+++ src/memoryfm/api/main.py
@@ -1,9 +1,42 @@
 from __future__ import annotations
+import os
+import asyncio
+from dotenv import load_dotenv
 from typing import Literal, Annotated
-from fastapi import FastAPI, Query
+from fastapi import (
+    BackgroundTasks,
+    FastAPI,
+    Query,
+    WebSocket,
+    WebSocketDisconnect,
+)
 from memoryfm.db_services.stats import get_user_summary, get_top_charts
+from memoryfm.io.lastfm_api import sync_lastfm_api, task_status
 
 app = FastAPI()
+load_dotenv()
+API_KEY = os.getenv("API_KEY", "")
+
+
+@app.post("/user/{username}/sync")
+async def sync_scrobbles(username: str, bg_tasks: BackgroundTasks):
+    task_status.clear()
+    task_status["status"] = "running"
+    bg_tasks.add_task(sync_lastfm_api, username, api_key=API_KEY)
+    return {"message": f"Syncing scrobbles for user: {username}"}
+
+
+@app.websocket("/ws/sync-progress")
+async def sync_progress_websocket(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            await websocket.send_json(task_status)
+            if task_status.get("status") == "completed":
+                break
+            await asyncio.sleep(1)
+    except WebSocketDisconnect:
+        print("Client disconnected.")
 
 
 @app.get("/user/{username}/summary")
