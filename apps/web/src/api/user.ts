a2f62d9ea1c588480a62550b133317ab6a6cedc0
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-10 23:27:38 +0530

    web: add web app api for fetching Summary and Recent Activity

---
+++ apps/web/src/api/user.ts
@@ -0,0 +1,13 @@
+const BASE_URL = "http://127.0.0.1:8000";
+
+export const fetchUserSummary = async (username: string) => {
+  const res = await fetch(`${BASE_URL}/user/${username}/summary`);
+  if (!res.ok) throw new Error("User summary not found");
+  return res.json();
+};
+
+export const fetchRecentActivity = async (username: string, weeks: number) => {
+  const res = await fetch(`${BASE_URL}/user/${username}/recent_scrobbles?weeks=${weeks}`);
+  if (!res.ok) throw new Error("Activity not found");
+  return res.json();
+};
