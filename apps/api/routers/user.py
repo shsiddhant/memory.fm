dbf0275a26e381253b3226850ea46ee545dd00ae
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-12 16:51:23 +0530

    api; use Pydantic StringConstraints to disallow empty space username
    - Add an input annotated type TrimmedStr of type str with strip
      whitespace and min length = 1 Pydantic StringConstraints.
    - Change username type annotation from str to TrimmedStr in user API
      routers.

--- apps/api/routers/user.py
+++ apps/api/routers/user.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 import os
 from dotenv import load_dotenv
-from fastapi import APIRouter, BackgroundTasks, Depends
+from fastapi import (
+    APIRouter,
+    BackgroundTasks,
+    Depends,
+)
 from fastapi.responses import JSONResponse
-
 from api.routers.websockets import task_status
 from api.response_models import RecentActivity, ScrobblesCount
 from memoryfm.io.lastfm_api import sync_lastfm_api
 from memoryfm.storage.db import get_db_session
 import memoryfm.services.stats_service as stserv
+from api.input_annotated_types import TrimmedStr  # noqa: TC001
 
 router = APIRouter()
 
@@ -18,7 +22,9 @@ API_KEY = os.getenv("API_KEY", "")
 
 @router.post("/user/{username}/sync")
 async def sync_scrobbles(
-    username: str, bg_tasks: BackgroundTasks, session=Depends(get_db_session)
+    username: TrimmedStr,
+    bg_tasks: BackgroundTasks,
+    session=Depends(get_db_session),
 ):
     task_status.clear()
     task_status["status"] = "running"
@@ -29,12 +35,16 @@ async def sync_scrobbles(
 
 
 @router.get("/user/{username}/summary")
-def summary(username: str, session=Depends(get_db_session)):
+def summary(username: TrimmedStr, session=Depends(get_db_session)):
     return stserv.get_summary_by_username(session, username)
 
 
 @router.get("/user/{username}/recent_scrobbles", response_model=RecentActivity)
-def recent_scrobbles(username: str, weeks: int = 8, session=Depends(get_db_session)):
+def recent_scrobbles(
+    username: TrimmedStr,
+    weeks: int = 8,
+    session=Depends(get_db_session),
+):
     data = stserv.get_daily_scrobbles_count(session, username, limit=weeks * 7)
     if data is not None:
         from_date, to_date, counts_seq = data
