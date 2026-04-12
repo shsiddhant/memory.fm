556dd51f0d38aecdabeab8a37c5ba9b2d42d60ab
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-12 10:34:15 +0530

    db: migrate database from SQLite to PostgresSQL
    - Migrate database to PostgreSQL.
    - Use env vars to get database config.
    - Add example env file.

--- src/memoryfm/storage/db.py
+++ src/memoryfm/storage/db.py
@@ -2,9 +2,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 import logging
 from contextlib import contextmanager
-from sqlalchemy import create_engine, event
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlite3 import Connection as SQLite3Connection
 
 from memoryfm.config import DB_URL
 from memoryfm.core.models import Base
@@ -18,25 +17,12 @@ logging.basicConfig()
 logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 
 
-@event.listens_for(engine, "connect")
-def _set_sqlite_pragma(dbapi_connection, connection_record):
-    if isinstance(dbapi_connection, SQLite3Connection):
-        cursor = dbapi_connection.cursor()
-        cursor.execute("PRAGMA foreign_keys=ON;")
-        cursor.close()
-
-
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 
 def init_db():
     "Initialize database from models schema."
     Base.metadata.create_all(engine)
-
-
-def startup():
-    "Application startup"
-    init_db()
 
 
 @contextmanager
