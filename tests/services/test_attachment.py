a0cdcd8384200ec44ea50be906ba1838342c7228
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-18 21:26:58 +0530

    tests: replace test dataset and add weighted attachement index test.
    - Use new dataset 'fiona.json' in tests.
    - Update tests and assert statements to reflect new dataset.
    - Add test for weighted attachement index service.

--- tests/services/test_attachment.py
+++ tests/services/test_attachment.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-from math import exp, log
+import numpy as np
 
 import pytest
 from memoryfm.models.service_enums import (
@@ -13,13 +13,44 @@ if TYPE_CHECKING:
     from sqlalchemy.orm import Session
 
 
-kind = ChartKindColumn.Album
+kind = ChartKindColumn.Track
 freq = Frequency.W
 username = "lazulinoother"
 from_ts, to_ts = None, None
 
-renyi_entropy_expected = {1: -(5 * log(5 / 6) + log(1 / 6)) / 6, 2: -log(26 / 36)}
-att_index_expected = {k: 100 * exp(-v) for k, v in renyi_entropy_expected.items()}
+
+# Scrobbles counts of distinct tracks on different days.
+daily_counts = np.array(
+    [
+        [1, 1, 3],  # Day 0
+        [4, 1, 1],  # Day 1
+    ]
+)
+# Scrobble Counts for distinct tracks
+# Purposely chosen data with no overlap in days, for simplicity.
+track_counts = np.array([1, 1, 3, 4, 1, 1])
+# Proportions
+props: np.typing.NDArray = track_counts / np.sum(track_counts)
+# Renyi Entropy - full history
+renyi_entropy_expected = {
+    1: -np.sum(props * np.log(props)),
+    2: -np.log(np.sum(props**2)),
+}
+# Attachment Index - full history
+att_index_expected = {k: 100 * np.exp(-v) for k, v in renyi_entropy_expected.items()}
+
+# Max total count was on Day 1 --> 6
+maxct = np.max(daily_counts.sum(axis=1))
+# Total count on Day 0 ---> 5
+day_0ct = np.sum(daily_counts[0])
+# Day 0 Weight --> log2(1 + 5/6)
+weight_0 = np.log1p(day_0ct / maxct) / np.log(2)
+# Day 0 proportions
+props_0 = np.array(daily_counts[0]) / np.sum(daily_counts[0])
+# Day 0 Renyi Entropy
+entropy_day_0 = -np.sum(props_0 * np.log(props_0))
+# Day 0 Weighted Attachment
+wtd_att_index_expected = weight_0 * 100 * np.exp(-entropy_day_0)
 
 
 def test_get_renyi_entropy(seeded_db: Session):
@@ -40,3 +71,17 @@ def test_get_attachment_index(seeded_db: Session):
         assert att_index is not None
         assert len(att_index) == 1
         assert att_index[0].get("value") == pytest.approx(value, rel=pow(10, -8))
+
+
+def test_get_weighted_attachment_index(seeded_db: Session):
+    wtd_att_index = attserv.get_weighted_attachment_index_by_username(
+        seeded_db,
+        username,
+        kind,
+        from_ts,
+        to_ts,
+        freq=Frequency.D,
+    )
+    assert wtd_att_index is not None
+    assert len(wtd_att_index) == 2
+    assert wtd_att_index[0].get("value") == pytest.approx(wtd_att_index_expected)
