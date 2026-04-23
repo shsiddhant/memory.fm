e5b0dcdb492d55e61cb88ba8bb4c05e638f82c5f
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-23 18:35:28 +0530

    test: update test after the `kind.value` -> `name` rename

--- tests/services/test_attachment.py
+++ tests/services/test_attachment.py
@@ -93,4 +93,4 @@ def test_get_attachment_moments(seeded_db: Session):
     )
     assert moments is not None
     assert len(moments) == 2
-    assert moments[1].get("track") == "Anything We Want"
+    assert moments[1].get("name") == "Anything We Want"
