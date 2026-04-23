7a7b4648169858d01e081dba2eb67bd95bbf2a57
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-23 18:25:02 +0530

    api: add attachment moments API endpoint
    - Add a FastAPI endpoint for the attachment moments service.
    - Introduce `AttachmentMoment` Pydantic model and use
      `Sequence[AttachmentMoment] as response model for the endpoint.
    - Rename 'z-score' and 'kind.value' in attachment moments to 'z_score'
      and 'name' to make response model easier to apply.

--- apps/api/routers/analytics.py
+++ apps/api/routers/analytics.py
@@ -2,7 +2,7 @@ import datetime
 from typing import Literal, Annotated, Sequence
 from fastapi import APIRouter, Depends, Query
 
-from api.response_models import TimeSeriesData, TopChart
+from api.response_models import AttachmentMoment, TimeSeriesData, TopChart
 from memoryfm.models.service_enums import ChartKindColumn, Frequency
 from memoryfm.services.user_service import get_user_context
 from memoryfm.storage.session import get_db_session
@@ -131,6 +131,72 @@ def attachment_index(
             normalize_timestamp(to_ts, tz),
             freq,
             alpha,
+        )
+        or []
+    )
+
+
+@router.get(
+    "/user/{username}/attachment_moments", response_model=Sequence[AttachmentMoment]
+)
+def attachment_moments(
+    username: str,
+    kind: Annotated[
+        ChartKindColumn, Query(description="Kind of Attachment Moments to fetch.")
+    ],
+    from_ts: Annotated[
+        datetime.datetime | None,
+        Query(
+            description=(
+                "Fetch attachment moments since this datetime. *Use ISO 8601 datetime.*"
+            )
+        ),
+    ] = None,
+    to_ts: Annotated[
+        datetime.datetime | None,
+        Query(
+            description=(
+                "Fetch attachment moments upto this datetime. *Use ISO 8601 datetime.*"
+            )
+        ),
+    ] = None,
+    freq: Annotated[
+        Frequency,
+        Query(
+            description="The Frequency over which scrobble counts are grouped together."
+        ),
+    ] = Frequency.D,
+    alpha: Annotated[
+        float,
+        Query(
+            description=(
+                "Order of Attachment Index to use for attachment moments. "
+                "The order is same as order of the underlying Rényi Entropy."
+            ),
+            ge=0.5,
+            le=3.0,
+        ),
+    ] = 1,
+    threshold: Annotated[
+        float,
+        Query(
+            description="Threshold z_score for choosing top attachment moments.", ge=1
+        ),
+    ] = 1,
+    session=Depends(get_db_session),
+):
+    """Fetch Attachment Moments for user in the given datetime range."""
+    tz = get_user_context(session, username).tz
+    return (
+        attserv.get_attachment_moments(
+            session,
+            username,
+            kind,
+            normalize_timestamp(from_ts, tz),
+            normalize_timestamp(to_ts, tz),
+            freq,
+            alpha,
+            threshold,
         )
         or []
     )
