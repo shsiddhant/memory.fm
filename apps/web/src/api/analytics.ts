63311af255fcbd2b273c8591b3e461bdf10d26eb
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-25 08:51:21 +0530

    web: add attachment moment type and fetch function
    - Add Attachment Moment type and move top chart interface to typing
      module.
    - Add function to fetch Attachment Moments from back-end.

---
+++ apps/web/src/api/analytics.ts
@@ -0,0 +1,31 @@
+const BACKEND_URL = "http://127.0.0.1:8000";
+import { handleResponse } from "@/api/user";
+import type { KindType } from "@/typing";
+
+const alpha = 2;
+const threshold = 1.5;
+
+export const fetchAttachmentMoments = async(
+    { username, kind, from_ts, to_ts }: {
+        username: string;
+        kind: KindType;
+        from_ts: string | null, // ISO 8601
+        to_ts: string | null,// ISO 8601
+
+    }
+) => {
+    const res = await fetch(
+        `${BACKEND_URL}/user/${username}/attachment_moments?kind=${kind}&from_ts=${from_ts}&to_ts=${to_ts}&freq=day&alpha=${alpha}&threshold=${threshold}`
+    );
+    return handleResponse(res);
+};
+
+export const fetchAttachmentMomentsByPeriod = async (
+    username: string, kind: string, period: number | "all_time"
+) => {
+    const res = await fetch(
+        `${BACKEND_URL}/user/${username}/attachment_moments_last?kind=${kind}&period=${period}&freq=day&alpha=${alpha}&threshold=${threshold}`
+    );
+    return handleResponse(res);
+}
+
