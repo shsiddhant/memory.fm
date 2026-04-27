adc5fcc747024c4dbb17dde0e769fa659e867104
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-04-27 11:02:23 +0530

    services: add service to calculate listening streaks
    - Add service to fetch consecutive comparison (artist / album / track).
    - Add cython module to calculate streaks from boolean comparison array.
    - Add service to compute listening streaks by timestamps and by year.
    - Add tests for streaks services.
    - Add cython dependency and setuptools configuration to compile the cython module.

---
+++ src/memoryfm/services/streaks_service.py
@@ -0,0 +1,48 @@
+import datetime
+
+from sqlalchemy.orm import Session
+
+from memoryfm.models.service_enums import ChartKindColumn
+import memoryfm.storage.streaks as strkrepo
+from memoryfm.storage.user_repo import get_user_by_username
+
+
+def get_streaks_by_username(
+    session: Session,
+    username: str,
+    kind: ChartKindColumn,
+    from_ts: datetime.datetime | None = None,
+    to_ts: datetime.datetime | None = None,
+    min_length: int = 2,
+):
+    user = get_user_by_username(session, username)
+    if user:
+        user_id = user.id
+        return strkrepo.get_streaks(
+            session,
+            user_id,
+            kind,
+            from_ts,
+            to_ts,
+            min_length,
+        )
+    return None
+
+
+def get_streaks_by_year(
+    session: Session,
+    username: str,
+    kind: ChartKindColumn,
+    year: int,
+    min_length: int = 2,
+):
+    from_ts = datetime.datetime(year=year, month=1, day=1)
+    to_ts = datetime.datetime(year=year, month=12, day=31)
+    return get_streaks_by_username(
+        session,
+        username,
+        kind,
+        from_ts,
+        to_ts,
+        min_length,
+    )
