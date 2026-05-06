068363d828f632e44fadf7528f1373a6c85cd407
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-05-06 18:27:40 +0530

    services: refactor cte and attachment services to fetch using `AnalyticsView`
    - Refactor following services to use AnalyticsView for fetching data, id
    columns to group by kind, and show subname.
      - `get_frequency_cte`
      - `get_frequency_proportions_cte`
      - `get_top_charts_by_freq`
      - `get_attachment_moments`

--- src/memoryfm/storage/cte_util.py
+++ src/memoryfm/storage/cte_util.py
@@ -2,7 +2,7 @@ import datetime
 
 from sqlalchemy import CTE, Float, func, select
 
-from memoryfm.models.core import Scrobble
+from memoryfm.models.core import AnalyticsView
 from memoryfm.models.service_enums import ChartKindColumn, Frequency
 
 
@@ -13,29 +13,43 @@ def get_frequency_cte(
     to_ts: datetime.datetime | None = None,
     freq: Frequency = Frequency.D,
 ) -> CTE:
-    stmt_bin = select(
-        func.date_trunc(freq.value, Scrobble.timestamp).label(freq.value),
-        kind.column,
-    ).where(Scrobble.user_id == user_id)
+    id_col = kind.id_column.label("id")  # type: ignore[attr-defined]
+    name_col = kind.name_column.label("name")  # type: ignore[attr-defined]
+    subname_col = kind.subname_column.label("subname") if kind.subname_column else None  # type: ignore[attr-defined]
+    date_col = func.date_trunc(freq.value, AnalyticsView.timestamp).label(freq.value)
+
+    if subname_col is not None:
+        stmt = select(date_col, id_col, name_col, subname_col)
+    else:
+        stmt = select(date_col, id_col, name_col)
+
+    stmt_bin = stmt.where(AnalyticsView.user_id == user_id)
+
     if from_ts:
-        stmt_bin = stmt_bin.filter(Scrobble.timestamp >= from_ts)
+        stmt_bin = stmt_bin.filter(AnalyticsView.timestamp >= from_ts)
     if to_ts:
-        stmt_bin = stmt_bin.filter(Scrobble.timestamp < to_ts)
+        stmt_bin = stmt_bin.filter(AnalyticsView.timestamp <= to_ts)
 
     stmt_cte_bin = stmt_bin.cte("binned")
 
-    stmt_cte_freq = (
-        select(
-            stmt_cte_bin.columns[freq.value],
-            stmt_cte_bin.columns[kind.value],
-            func.count().cast(Float).label("scrobbles"),
-        )
-        .group_by(
-            stmt_cte_bin.columns[freq.value],
-            stmt_cte_bin.columns[kind.value],
-        )
-        .cte("freq")
+    columns_new = (
+        stmt_cte_bin.columns[freq.value],
+        stmt_cte_bin.columns["id"],
+        func.any_value(stmt_cte_bin.columns["name"]).label("name"),
+        func.count().cast(Float).label("scrobbles"),
     )
+    if subname_col is not None:
+        stmt = select(
+            *columns_new,
+            func.any_value(stmt_cte_bin.columns["subname"]).label("subname"),
+        )
+    else:
+        stmt = select(*columns_new)
+
+    stmt_cte_freq = stmt.group_by(
+        stmt_cte_bin.columns[freq.value],
+        stmt_cte_bin.columns["id"],
+    ).cte("freq")
     return stmt_cte_freq
 
 
@@ -52,10 +66,19 @@ def get_frequency_proportions_cte(
     total_scrobbles_col = func.sum(scrobbles_col).over(
         partition_by=cte_freq_cols[freq.value]
     )
-    stmt_cte_props = select(
+
+    columns = (
         cte_freq_cols[freq.value],
-        cte_freq_cols[kind.value],
+        cte_freq_cols["id"],
+        cte_freq_cols["name"],
         total_scrobbles_col.label("total_scrobbles"),
         (scrobbles_col / total_scrobbles_col).label("prop"),
-    ).cte("props")
+    )
+
+    if kind.subname_column is not None:
+        stmt_cte_props = select(*columns, cte_freq_cols["subname"]).cte("props")
+    else:
+        stmt_cte_props = select(*columns).cte("props")
+
+    stmt_cte_props = select(*columns).cte("props")
     return stmt_cte_props
