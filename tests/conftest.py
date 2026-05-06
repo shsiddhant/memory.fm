5ba25a572c30b5afec872bcd35db64d105f36977
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-05-06 16:36:27 +0530

    test: create analytics view and use in lastfmstats tests
    - Create analytics view in engine configuration.
    - Replace `Scrobble` with `AnalyticsView` in `TestFromLastfmstats`
      tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, DDL
 from sqlalchemy.orm import sessionmaker
 from testcontainers.postgres import PostgresContainer
 from memoryfm.io.lastfmstats import from_lastfmstats
 from memoryfm.models.core import Base
+
 
 data_dir = Path(__file__).resolve().parent / "data"
 json_dir = data_dir / "json"
@@ -23,7 +23,42 @@ def postgres_container():
 @pytest.fixture(scope="session")
 def engine(postgres_container):
     engine = create_engine(postgres_container.get_connection_url())
+
+    view_name = "analytics_view"
+    view_table = Base.metadata.tables.get(view_name)
+
+    if view_table is not None:
+        Base.metadata._remove_table(view_name, schema=view_table.schema)
+
     Base.metadata.create_all(engine)
+
+    if view_table is not None:
+        Base.metadata.tables._insert_item(view_name, view_table)
+
+    view_ddl = DDL("""
+        CREATE VIEW analytics_view AS
+        SELECT
+            s.id AS scrobble_id,
+            s.timestamp,
+            u.id AS user_id,
+            u.username,
+            t.id AS track_id,
+            t.name AS track,
+            al.id AS album_id,
+            al.name AS album,
+            ar.id AS artist_id,
+            ar.name AS artist
+        FROM scrobbles s
+        JOIN users u ON s.user_id = u.id
+        JOIN tracks t ON s.track_id = t.id
+        JOIN albums al ON t.album_id = al.id
+        JOIN artists ar ON t.artist_id = ar.id
+    """)
+
+    with engine.connect() as conn:
+        conn.execute(view_ddl)
+        conn.commit()
+
     yield engine
 
     engine.dispose()
