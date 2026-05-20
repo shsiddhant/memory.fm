69c82c7e107ed3e15dd0069c4b823ff8691e47d8
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-05-20 18:29:46 +0530

    web: improve sync progress and completed alert ui.
    - Fix widths of sync progress, error alert, and completed alert to make
      them more usable on smaller screens.
    - Show fetched scrobbles status below sync progress bar.
    - Fix scrobble/scrobbles in completed alert based on fetched scrobbles
      count.

--- apps/web/src/components/features/overview/sync_complete_alert.tsx
+++ apps/web/src/components/features/overview/sync_complete_alert.tsx
@@ -9,20 +9,20 @@ export default function SyncCompleteAlert(
     return (
         <Flex
             direction={"column"}
-            padding={5}
+            padding={{base: 2, md: 5 }}
             mb={"6"}
             align="center"
             alignItems={"center"}
         >
-            <Alert.Root status={"info"} width={"sm"}>
+            <Alert.Root status={"info"} width={{ base: "250px", md: "xs"}}>
                 <Alert.Indicator />
                 <Alert.Content>
-                    <Alert.Title>Fetched {fetched! - 1} scrobble{fetched! == 1 ? "s" : ""}.</Alert.Title>
+                    <Alert.Title>Fetched {fetched! - 1} scrobble{fetched! != 1 ? "s" : ""}</Alert.Title>
                 </Alert.Content>
                 <CloseButton
                     position="absolute"
-                    right="1" 
-                    top="1" 
+                    right="-1"
+                    top="1"
                     onClick={() => onClose()} />
             </Alert.Root>
         </Flex>
