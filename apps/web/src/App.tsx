546b6a967c2763f887232055123bd3f485631f0b
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-06-03 07:15:08 +0530

    web: add maintenance page and env variable.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,19 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { Box } from '@chakra-ui/react';
+import Maintenance from '@/components/maintenance';
 
 export default function App() {
+
+  const isMaintenanceMode = import.meta.env.VITE_APP_MAINTENANCE === 'true';
+
+  if (isMaintenanceMode) {
+    return <Maintenance />;
+  }
+  
   return (
     <Box minH="100vh" bg="bg.panel">
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </Box>
   );
 }
